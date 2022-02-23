f8c11a08b8cbdb6949f9d9eaf8caa72b568a1891
George Adams <georgeadams1995@gmail.com>
2022-02-23 15:06:04 +0000

    add donate button (#153)
    * add donate button
    
    * linter fixes

--- src/pages/download.js
+++ src/pages/download.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import { useQueryParam, StringParam } from 'use-query-params'
+import { BiDonateHeart } from 'react-icons/bi'
 
 import Layout from '../components/Layout'
 import Seo from '../components/Seo'
@@ -24,9 +25,11 @@ const DownloadPage = () => {
         <div className='row py-lg-5'>
           <div className='col-lg-10 col-md-8 mx-auto'>
             <h1 className='fw-light'>Thank you for your download!</h1>
-            {vendor === 'Temurin'
-              ? <p className='py-2'>You are downloading an Eclipse Temurin build, the open-source community build from the Eclipse Adoptium Working Group.</p>
-              : <p className='py-2'>You are downloading a build from <strong>{vendor}</strong>, a member of the Eclipse Adoptium Working Group.</p>}
+            {link && (
+              vendor === 'Temurin'
+                ? <p className='py-2'>You are downloading an Eclipse Temurin build, the open-source community build from the Eclipse Adoptium Working Group.</p>
+                : <p className='py-2'>You are downloading a build from <strong>{vendor}</strong>, a member of the Eclipse Adoptium Working Group.</p>
+            )}
 
             {link && <meta http-equiv='refresh' content={`0; url=${link}`} />}
             {link
@@ -36,7 +39,8 @@ const DownloadPage = () => {
             {vendor === 'Temurin' && <p>Eclipse Temurin binaries are 100% free and open source and used by millions of developers every day. Here are three easy ways you can contribute toward the future development of Eclipse Adoptium projects and technologies.</p>}
             <p>Here are three easy ways you can contribute towards the future development of Eclipse Adoptium projects and technologies.</p>
             <h2 className='fw-light py-3'>Donate to the Eclipse Adoptium Working Group</h2>
-            <p>You can make a one-time or regular targetted donation to the Adoptium Working Group by filling in the <a href='https://www.eclipse.org/org/workinggroups/sponsorship/working-group-sponsorship-agreement.pdf'>Working Group Sponsorship Agreement.</a></p>
+            <p>You can help power the Adoptium Community with a donation</p>
+            <a href='https://eclipse.org/donate/adoptium' target='_blank' className='btn btn-primary btn-lg' rel='noreferrer'>Donate <BiDonateHeart /></a>
             <h2 className='fw-light py-3'>Get involved</h2>
             <p>Whether you choose to report bugs, request features, or become a committer, you can help improve the technology for yourself and the rest of the community.</p>
             <h2 className='fw-light py-3'>Join the Working Group</h2>

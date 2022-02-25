04a6399d4431e8d33108d21cab65192d3cfcaab6
Tim Ellison <tellison@users.noreply.github.com>
2022-02-25 13:03:58 +0000

    Updates to product listing information (#169)
    * Rename src/asciidoc-pages/docs/marketplace-guide.adoc to src/asciidoc-pages/docs/marketplace-guide/index.adoc
    
    * Rename src/asciidoc-pages/docs/marketplace-policy.adoc to src/asciidoc-pages/docs/marketplace-policy/index.adoc
    
    * Additional links
    
    * Update index.adoc
    
    * Add links to QVSL and Publisher Agreement
    
    * Update index.adoc
    
    * Add link to EFQVSL
    
    * Add full name
    
    * Start to breakout the repository format
    
    * Create marketplace-listing.adoc
    
    * Add JSON syntax highlighting
    
    * Update src/asciidoc-pages/docs/marketplace-listing.adoc
    
    * Bank changes so far
    
    * Update marketplace-listing.adoc
    
    * Update marketplace-listing.adoc
    
    * Fix johnoliver attribution, and minor tweaks
    
    * Add some xrefs
    
    * Link to repo description
    
    * Update xref
    
    This may require adding an relfilesuffix attribute to make it work
    
    * Apply suggestions from code review
    
    * Update src/asciidoc-pages/docs/marketplace-guide/index.adoc
    
    Co-authored-by: George Adams <george.adams@microsoft.com>

--- src/util/highlightCode.ts
+++ src/util/highlightCode.ts
@@ -4,12 +4,14 @@ import 'highlight.js/styles/atom-one-dark.css'
 import java from 'highlight.js/lib/languages/java'
 import bash from 'highlight.js/lib/languages/bash'
 import powershell from 'highlight.js/lib/languages/powershell'
+import json from 'highlight.js/lib/languages/json'
 import dos from 'highlight.js/lib/languages/dos'
 
 hljs.registerLanguage('java', java)
 hljs.registerLanguage('bash', bash)
 hljs.registerLanguage('powershell', powershell)
 hljs.registerLanguage('dos', dos)
+hljs.registerLanguage('json', json)
 
 export default function highlightCode () {
   const codeBlocks = document.querySelectorAll('pre > code')

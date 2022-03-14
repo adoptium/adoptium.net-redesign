defcc402818490d3d67190c93d3c44e8259a5b07
George Adams <georgeadams1995@gmail.com>
2022-03-14 19:34:12 +0000

    Update highlightCode.ts

--- src/util/highlightCode.ts
+++ src/util/highlightCode.ts
@@ -1,6 +1,7 @@
 import hljs from 'highlight.js/lib/core'
 import 'highlight.js/styles/atom-one-dark.css'
 
+// See the full list of supported languages at https://highlightjs.readthedocs.io/en/latest/supported-languages.html
 import java from 'highlight.js/lib/languages/java'
 import bash from 'highlight.js/lib/languages/bash'
 import powershell from 'highlight.js/lib/languages/powershell'

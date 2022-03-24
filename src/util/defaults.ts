7d14566ac10ded9f7545dab592892e47f1a9d7eb
George Adams <georgeadams1995@gmail.com>
2022-03-24 19:05:38 +0000

    Update defaults.ts

--- src/util/defaults.ts
+++ src/util/defaults.ts
@@ -3,7 +3,7 @@
 export const oses = ['Linux', 'Alpine', 'Windows', 'macOS', 'AIX', 'Solaris']
 export const arches = ['x64', 'x86', 'aarch64', 's390x', 'ppc64le', 'ppc64', 'arm', 'sparcv9']
 export const packageTypes = ['JDK', 'JRE']
-export const versions = [17, 16, 11, 8]
+export const versions = [18, 17, 16, 11, 8]
 export const marketplaceDistributions = ['microsoft', 'temurin', 'zulu', 'semeru_certified']
 // The default JDK version to serve up on pages
 export const defaultVersion = 17

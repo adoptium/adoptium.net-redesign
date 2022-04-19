85e464cf270fdda260ea9f4de6c0e54bc664742f
Tim Ellison <tellison@users.noreply.github.com>
2022-04-19 17:20:39 +0100

    Add current release information to page (#399)
    Add some explanation text to the current release download page.
    Move archive link to bottom of page.

--- src/pages/temurin/releases.js
+++ src/pages/temurin/releases.js
@@ -15,18 +15,43 @@ const TemurinReleases = () => (
     <section className='py-5 text-center container'>
       <div className='row py-lg-5'>
         <div className='col-lg-10 col-md-8 mx-auto'>
-          <h1 className='fw-light'>Latest releases</h1>
+          <h1 className='fw-light'>Eclipse Temurin™ Latest Releases</h1>
           <div className='row align-items-center pt-3'>
-            <div className='btn-group-vertical col-6 mx-auto'>
-              <Link to='/temurin/archive' className='btn btn btn-primary mt-3'>
-                Build archive <FaArrowCircleRight />
-              </Link>
+            <div className='col-6 col-md-4'>
+              <img
+                src='../../images/temurin-light.png'
+                width={120}
+                alt='Temurin logo'
+                className='img-fluid'
+              />
+            </div>
+            <div className='col-12 col-sm-6 col-md-8'>
+              <p className='text-start'>
+                Eclipse Temurin is the open source Java SE build based upon OpenJDK.
+                Temurin is available for a <a href='/supported-platforms'>wide range of platforms</a> and Java SE versions.
+                The latest releases recommended for use in production are listed below, and are regularly <a href='/support'>updated and supported</a> by the Adoptium community. Migration help, container images and package installation guides are available in the <a href='/docs'>documentation section</a>.
+              </p>
             </div>
           </div>
         </div>
       </div>
+      <div className='row align-items-center pt-3'>
+        <p className='text-center'>
+          Use the drop-down boxes below to filter the list of current releases.
+        </p>
+      </div>
       <DownloadDropdowns updaterAction={loadLatestAssets} marketplace={false} Table={DownloadTable} />
       <ChecksumModal />
+      <div className='row align-items-center pt-3'>
+        <p className='text-center'>
+          Previous releases are available in the Temurin archive.
+        </p>
+        <div className='btn-group-vertical col-6 mx-auto'>
+          <Link to='/temurin/archive' className='btn btn btn-primary mt-3'>
+            Build archive <FaArrowCircleRight />
+          </Link>
+        </div>
+      </div>
     </section>
   </Layout>
 )

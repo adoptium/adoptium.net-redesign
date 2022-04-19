46a5144b359a84575a190ae79eea6a32cdd62e89
Haroon Khel <34969545+Haroon-Khel@users.noreply.github.com>
2022-04-19 15:37:10 +0100

    Add April 2022 release banner (#408)
    * add april 2022 release banner
    
    * add classnames to links
    
    * correct date

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,19 +3,10 @@ import React from 'react';
 const Banner = () => {
   return (
     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-        <strong className='p-1'>7th April 2022:</strong>
-            Eclipse Temurin binaries for JDK18 GA for 10 platforms are available 
-            via the 
-            <a href="https://adoptium.net/temurin/releases"
-            className="alert-link p-1 text-white"
-            >
-              website
-            </a> and the 
-            <a href="https://api.adoptium.net/"
-            className="alert-link p-1 text-white"
-            >
-              Adoptium API
-            </a>.
+        <strong className='p-1'>19th April 2022:</strong>
+          We are creating the April 2022 CPU binaries for Eclipse Temurin 8u332, 11.0.15 and 17.0.3 and 18.0.1<br/>
+          You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/140">by platform</a> 
+          or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/139">by detailed release checklist</a>.
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
   );

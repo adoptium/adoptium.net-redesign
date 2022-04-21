0685050692b83f53dfb87ab3b2a188ed33ab7f90
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2022-04-21 18:34:54 +0100

    Switch CPU to PSU in the April banner (#420)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
         <strong className='p-1'>19th April 2022:</strong>
-          We are creating the April 2022 CPU binaries for Eclipse Temurin 8u332, 11.0.15 and 17.0.3 and 18.0.1<br/>
+          We are creating the April 2022 PSU binaries for Eclipse Temurin 8u332, 11.0.15 and 17.0.3 and 18.0.1<br/>
           You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/140">by platform</a> 
           or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/139">by detailed release checklist</a>.
         <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>

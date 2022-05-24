804f564c2e6e2e627d28fcc4837d0b773a5d6689
George Adams <georgeadams1995@gmail.com>
2022-05-24 16:33:49 +0100

    fix vendor in download.js

--- src/pages/download.js
+++ src/pages/download.js
@@ -40,7 +40,7 @@ const DownloadPage = ({ location }) => {
           <div className='col-lg-10 col-md-8 mx-auto'>
             <h1 className='fw-light'>Thank you for your download!</h1>
             {link && (
-              vendor === 'Temurin'
+              vendor === 'Adoptium'
                 ? <p className='py-2'>You are downloading an Eclipse Temurin build, the open-source community build from the Eclipse Adoptium Working Group.</p>
                 : (
                   <>
@@ -59,7 +59,7 @@ const DownloadPage = ({ location }) => {
               ? <p className='text-muted py-3'>If the download doesn't start in a few seconds, please <a href={link}>click here</a> to start the download.</p>
               : <p className='text-muted py-3'>Ooops - something doesn't seem quite right here. Please try again.</p>}
 
-            {vendor === 'Temurin' && <p>Eclipse Temurin binaries are 100% free and open source and used by millions of developers every day. Here are three easy ways you can contribute toward the future development of Eclipse Adoptium projects and technologies.</p>}
+            {vendor === 'Adoptium' && <p>Eclipse Temurin binaries are 100% free and open source and used by millions of developers every day. Here are three easy ways you can contribute toward the future development of Eclipse Adoptium projects and technologies.</p>}
             <h2 className='fw-light py-3'>Donate to the Eclipse Adoptium Working Group</h2>
             <p>You can help power the Adoptium Community with a donation</p>
             <a href='https://eclipse.org/donate/adoptium' target='_blank' className='btn btn-primary btn-lg' rel='noreferrer'>Donate <BiDonateHeart /></a>

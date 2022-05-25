89f81587c41b3c3b211c460ce92c351e4cf4a2f4
Tim Ellison <tellison@users.noreply.github.com>
2022-05-25 20:21:58 +0100

    Tweak text on marketplace page (#578)

--- src/pages/marketplace.js
+++ src/pages/marketplace.js
@@ -17,7 +17,7 @@ const DownloadPage = () => (
           <h1 className='fw-light'>Adoptium Marketplace</h1>
           <p className='pt-3'>
             Java™ is the world's leading programming language and platform.
-            The Adoptium Marketplace promotes high-quality, TCK certified runtimes and associated technology for use across the Java™ ecosystem.
+            The Adoptium Marketplace promotes high-quality, TCK certified and AQAvit verified runtimes for use across the Java ecosystem.
           </p>
           <div className='row align-items-center pt-3'>
             <div className='col-6 col-md-4'>
@@ -30,8 +30,8 @@ const DownloadPage = () => (
             </div>
             <div className='col-12 col-sm-6 col-md-8 '>
               <p className='text-start'>
-                The AQAvit test framework (<strong>A</strong>doptium <strong>Q</strong>uality <strong>A</strong>ssurance) can be found <a href='https://github.com/Adoptium/aqa-tests'>here</a>.
-                There is also a <a href='https://blog.adoptopenjdk.net/2017/12/testing-java-help-count-ways/'>blog post and brief presentation</a> that explains what testing we run and how it fits into the overall delivery pipeline.
+                The AQAvit open source test suite (<strong>A</strong>doptium <strong>Q</strong>uality <strong>A</strong>ssurance) can be found <a href='https://github.com/Adoptium/aqa-tests'>here</a>.
+                There is also a <a href='https://blog.adoptopenjdk.net/2017/12/testing-java-help-count-ways/'>blog post and brief presentation</a> that explains what testing is run and how it fits into the overall delivery pipeline.
               </p>
             </div>
           </div>

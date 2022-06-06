b2abdda69c08bdbe20cd14f331edba1fdffa742b
George Adams <georgeadams1995@gmail.com>
2022-06-06 19:12:54 +0300

    fix SSRProvider warning at build (#612)
    * fix SSRProvider warning at build
    
    * linter fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import { SSRProvider } from '@react-aria/ssr'
 
 import Navbar from './Navbar'
 import Banner from './Banner'
@@ -19,21 +20,23 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div
-      className='container-fluid p-0'
-      style={{
-        backgroundColor: 'var(--bg)',
-        color: 'var(--text-normal)',
-        transition: 'color 0.2s ease-out, background 0.2s ease-out'
-      }}
-    >
-      <Navbar siteTitle={data.site.siteMetadata?.title || 'Title'} />
-      <Banner />
-      <main>
-        <main>{children}</main>
-      </main>
-      <Footer />
-    </div>
+    <SSRProvider>
+      <div
+        className='container-fluid p-0'
+        style={{
+          backgroundColor: 'var(--bg)',
+          color: 'var(--text-normal)',
+          transition: 'color 0.2s ease-out, background 0.2s ease-out'
+        }}
+      >
+        <Navbar siteTitle={data.site.siteMetadata?.title || 'Title'} />
+        <Banner />
+        <main>
+          <main>{children}</main>
+        </main>
+        <Footer />
+      </div>
+    </SSRProvider>
   )
 }
 

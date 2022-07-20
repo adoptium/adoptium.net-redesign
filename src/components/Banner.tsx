e90ad70674133908d6de4c5048ad68d9ae20bb25
sophia-guo <sophia.gwf@gmail.com>
2022-07-20 12:15:36 -0400

    Add July 2022 release banner (#830)
    Signed-off-by: Sophia Guo <sophia.gwf@gmail.com>

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Banner = () => {
-  return null;
-//return (
-//  <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-//      <strong className='p-1'>19th April 2022:</strong>
-//        We are creating the April 2022 PSU binaries for Eclipse Temurin 8u332, 11.0.15 and 17.0.3 and 18.0.1<br/>
-//        You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/140">by platform</a> 
-//        or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/139">by detailed release checklist</a>.
-//      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-//  </div>
-//);
+
+  return (
+    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+     <strong className='p-1'>19th July 2022:</strong>
+       We are creating the July 2022 PSU binaries for Eclipse Temurin 8u342, 11.0.16 and 17.0.4 and 18.0.2<br/>
+       You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
+       or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/152">by detailed release checklist</a>.
+     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  );
 };
 
 export default Banner;

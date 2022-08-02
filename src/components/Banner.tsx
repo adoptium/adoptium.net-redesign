6ce1ccd78d2af605e1d8bd57f179120912f2420b
Martijn Verburg <martijnverburg@gmail.com>
2022-08-02 13:53:50 +0100

    update banner for 8u345 status (#858)
    * update banner for 8u345 status
    
    * update banner for 8u345 status
    
    * update banner for 8u345 status

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,9 +4,10 @@ const Banner = () => {
 
   return (
     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-     <strong className='p-1'>19th July 2022:</strong>
-       We are creating the July 2022 PSU binaries for Eclipse Temurin 8u342, 11.0.16 and 17.0.4 and 18.0.2<br/>
-       You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
+     <strong className='p-1'>2nd of August 2022:</strong>
+       We are going to replace the 8u342 binaries with a 8u345 release to fix <a href="https://bugs.openjdk.org/browse/JDK-8290832">JDK-8290832</a>. <br/>
+       This will extend our release cycle by a few more days.
+       You can continue to track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
        or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/152">by detailed release checklist</a>.
      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>

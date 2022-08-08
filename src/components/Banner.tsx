733627c8de489e78c7869c54f190a3c10ff089cb
George Adams <georgeadams1995@gmail.com>
2022-08-08 11:03:20 +0100

    fix class name in banner (#891)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
      <strong className='p-1'>2nd of August 2022:</strong>
-       We are going to replace the 8u342 binaries with a 8u345 release to fix <a href="https://bugs.openjdk.org/browse/JDK-8290832">JDK-8290832</a>. <br/>
+       We are going to replace the 8u342 binaries with a 8u345 release to fix <a className='alert-link p-1 text-white' href="https://bugs.openjdk.org/browse/JDK-8290832">JDK-8290832</a>. <br/>
        This will extend our release cycle by a few more days.
        You can continue to track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
        or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/152">by detailed release checklist</a>.

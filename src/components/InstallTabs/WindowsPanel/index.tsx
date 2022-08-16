30c13e518f13c1eb0c3927ea7352be455ae2af70
Bill ZHANG <36790218+Lutra-Fs@users.noreply.github.com>
2022-08-16 23:08:32 +1000

    fix: change the winget package name (#911)
    * fix: change the winget package name
    
    see https://github.com/microsoft/winget-pkgs/issues/67708
    
    * fix (snap): update snap to reflect code changes

--- src/components/InstallTabs/WindowsPanel/index.tsx
+++ src/components/InstallTabs/WindowsPanel/index.tsx
@@ -9,28 +9,28 @@ const WindowsPanel = (): JSX.Element => {
         <span className="install__text__no-select"># Install the latest version</span>
         <br />
         <span className="install__text__no-select">$</span>
-        <span className="install-text-command">winget install </span>EclipseAdoptium.Temurin.17
+        <span className="install-text-command">winget install </span>EclipseAdoptium.Temurin.17.JDK
       </ShellBox>
       <br />
       <ShellBox>
         <span className="install__text__no-select"># Install a different version</span>
         <br />
         <span className="install__text__no-select">$</span>
-        <span className="install-text-command">winget install </span>EclipseAdoptium.Temurin.11
+        <span className="install-text-command">winget install </span>EclipseAdoptium.Temurin.11.JDK
       </ShellBox>
       <br />
       <ShellBox>
         <span className="install__text__no-select"># Upgrade the Winget package</span>
         <br />
         <span className="install__text__no-select">$</span>
-        <span className="install-text-command">winget upgrade </span>EclipseAdoptium.Temurin.17
+        <span className="install-text-command">winget upgrade </span>EclipseAdoptium.Temurin.17.JDK
       </ShellBox>
       <br />
       <ShellBox>
         <span className="install__text__no-select"># Uninstall the Winget package</span>
         <br />
         <span className="install__text__no-select">$</span>
-        <span className="install-text-command">winget uninstall </span>EclipseAdoptium.Temurin.17
+        <span className="install-text-command">winget uninstall </span>EclipseAdoptium.Temurin.17.JDK
       </ShellBox>
       <a
         className="install__docs-button"

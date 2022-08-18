35e5851b66b50cc32f4e3149b48f59ca3522b42d
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2022-08-18 18:00:16 +0100

    Add banner to publicise 11.0.16.1 and 17.0.4.1 respins (#915)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 const Banner = () => {
-  return null;
-//   return (
-//     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-//      <strong className='p-1'>2nd of August 2022:</strong>
-//        We are going to replace the 8u342 binaries with a 8u345 release to fix <a className='alert-link p-1 text-white' href="https://bugs.openjdk.org/browse/JDK-8290832">JDK-8290832</a>. <br/>
-//        This will extend our release cycle by a few more days.
-//        You can continue to track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
-//        or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/152">by detailed release checklist</a>.
-//      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-//     </div>
-//   );
+
+  return (
+    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+     <strong className='p-1'>18th of August 2022:</strong>
+       Upstream OpenJDK has released fixes to
+       <a href="https://mail.openjdk.org/pipermail/jdk-updates-dev/2022-August/016460.html">
+          resolve a regression</a> in some of the July releases. We are working to release 11.0.16.1+1 and 17.0.4.1+1.
+       As usual, you can continue to track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
+       or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/152">by detailed release checklist</a>.
+     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  );
 };
 
 export default Banner;

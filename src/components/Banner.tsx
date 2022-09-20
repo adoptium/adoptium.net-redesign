93a91a469b80c94fffc45838a2b0ae4fe8404fc8
George Adams <georgeadams1995@gmail.com>
2022-09-20 11:27:26 +0100

    add September release banner (#1034)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 
 const Banner = () => {
-    return null;
+  // return null;
 
-  // return (
-  //   <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-  //    <strong className='p-1'>18th of August 2022:</strong>
-
-  //      Our 11.0.16.1 and 17.0.4.1 releases to <a className='alert-link p-1 text-white' href="https://mail.openjdk.org/pipermail/jdk-updates-dev/2022-August/016460.html">resolve a regression</a> in the
-  //      OpenJDK codebase are now available. We are now working on building a
-  //      JDK18 rebuild of 18.0.2.1 with the same fix.
-  //      As usual, you can continue to track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/153">by platform</a> 
-  //      or <a className='alert-link p-1 text-white' href="https://github.com/adoptium/adoptium/issues/152">by detailed release checklist</a>.
-  //    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  //   </div>
-  // );
+  return (
+    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+     <strong className='p-1'>20th of September 2022:</strong>
+        The Eclipse Temurin 19 GA Release effort has begun! You can track progress
+        <a
+          href="https://github.com/adoptium/adoptium/issues/170"
+          target='_blank'
+          rel='noopener noreferrer'
+          className="alert-link p-1 text-white"
+        >by platform</a> 
+        or 
+        <a 
+          href="https://github.com/adoptium/adoptium/issues/171"
+          target='_blank'
+          rel='noopener noreferrer'
+          className="alert-link p-1 text-white"
+        >by detailed release checklist</a>.
+     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  );
 };
 
 export default Banner;

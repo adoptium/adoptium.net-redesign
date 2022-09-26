14abb7c0b7660b12845630d8842b0949ca33f0a1
George Adams <georgeadams1995@gmail.com>
2022-09-26 11:30:33 +0100

    migrate to react18 (#1054)

--- src/hooks/__tests__/fetchLatestTemurin.test.tsx
+++ src/hooks/__tests__/fetchLatestTemurin.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, waitFor } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 import { fetchLatestForOS } from '../fetchLatestTemurin';
 import { mockLatestTemurin  } from '../../__fixtures__/hooks';
@@ -15,18 +15,20 @@ afterEach(() => {
 
 describe('fetchLatestForOS', () => {
   it('binary URL is set correctly', async () => {
-    const { result, waitForNextUpdate } = renderHook(() => fetchLatestForOS(true, 11, 'linux', 'x64'));
-    await waitForNextUpdate()
-    expect(global.fetch).toHaveBeenCalledTimes(1)
-    expect(result.current?.release_name).toBe('release_name_mock')
-    expect(result.current?.link).toEqual(new URL('https://link_mock/'))
+    const { result } = renderHook(() => fetchLatestForOS(true, 11, 'linux', 'x64'));
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+      expect(result.current?.release_name).toBe('release_name_mock')
+      expect(result.current?.link).toEqual(new URL('https://link_mock/'))
+    }, { interval: 1 });
   })
 
   it('installer is returned if available', async () => {
     mockResponse = [mockLatestTemurin(true)];
-    const { result, waitForNextUpdate } = renderHook(() => fetchLatestForOS(true, 11, 'linux', 'x64'));
-    await waitForNextUpdate()
-    expect(global.fetch).toHaveBeenCalledTimes(1)
-    expect(result.current?.link).toEqual(new URL('https://installer_link_mock'))
+    const { result } = renderHook(() => fetchLatestForOS(true, 11, 'linux', 'x64'));
+    await waitFor(() => {
+      expect(global.fetch).toHaveBeenCalledTimes(1)
+      expect(result.current?.link).toEqual(new URL('https://installer_link_mock'))
+    }, { interval: 1 });
   })
 });

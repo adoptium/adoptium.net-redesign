555d976a0c2cde792f9ee47848c6ea858766a6b0
George Adams <georgeadams1995@gmail.com>
2022-10-04 14:32:13 +0100

    remove website banner (#1086)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
 const Banner = () => {
-  // return null;
+  return null;
 
-  return (
-    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-     <strong className='p-1'>20th of September 2022:</strong>
-        The Eclipse Temurin 19 GA Release effort has begun! You can track progress
-        <a
-          href="https://github.com/adoptium/adoptium/issues/170"
-          target='_blank'
-          rel='noopener noreferrer'
-          className="alert-link p-1 text-white"
-        >by platform</a> 
-        or 
-        <a 
-          href="https://github.com/adoptium/adoptium/issues/171"
-          target='_blank'
-          rel='noopener noreferrer'
-          className="alert-link p-1 text-white"
-        >by detailed release checklist</a>.
-     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-  );
+//   return (
+//     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+//      <strong className='p-1'>20th of September 2022:</strong>
+//         The Eclipse Temurin 19 GA Release effort has begun! You can track progress
+//         <a
+//           href="https://github.com/adoptium/adoptium/issues/170"
+//           target='_blank'
+//           rel='noopener noreferrer'
+//           className="alert-link p-1 text-white"
+//         >by platform</a> 
+//         or 
+//         <a 
+//           href="https://github.com/adoptium/adoptium/issues/171"
+//           target='_blank'
+//           rel='noopener noreferrer'
+//           className="alert-link p-1 text-white"
+//         >by detailed release checklist</a>.
+//      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+//     </div>
+//   );
 };
 
 export default Banner;

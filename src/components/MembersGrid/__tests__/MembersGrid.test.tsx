5509fdda3588cd480c44167a623275ce68826262
George Adams <georgeadams1995@gmail.com>
2022-10-26 12:47:15 +0200

    adjust logo spacing on members page (#1119)

--- src/components/MembersGrid/__tests__/MembersGrid.test.tsx
+++ src/components/MembersGrid/__tests__/MembersGrid.test.tsx
@@ -14,6 +14,7 @@ describe('MembersGrid component', () => {
     {
         name: 'Member 2',
         logo: 'member2.png',
+        logoPadding: '1em',
         url: 'https://member2.com'
     }
   ]

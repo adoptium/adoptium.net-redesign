80a6b6839bac436313da658b2092ff02337d9b04
George Adams <georgeadams1995@gmail.com>
2022-11-28 14:41:02 +0000

    fix SSRProvider warnings in build logs (#1246)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import SSRProvider from 'react-bootstrap/SSRProvider'
 
 import NavBar from '../NavBar'
 import Banner from '../Banner'
@@ -19,21 +20,23 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div
-      className='container-fluid p-0'
-      style={{
-        backgroundColor: 'var(--bg)',
-        color: 'var(--text-normal)',
-        transition: 'color 0.2s ease-out, background 0.2s ease-out'
-      }}
-    >
-      <NavBar />
-      <Banner />
-      <main>
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <SSRProvider>
+      <div
+        className='container-fluid p-0'
+        style={{
+          backgroundColor: 'var(--bg)',
+          color: 'var(--text-normal)',
+          transition: 'color 0.2s ease-out, background 0.2s ease-out'
+        }}
+      >
+        <NavBar />
+        <Banner />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </SSRProvider>
   )
 }
 

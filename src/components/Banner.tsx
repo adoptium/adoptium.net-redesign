be94893da4f078b677fbf36a8329801bd0b890ab
Scott Fryer <60462088+steelhead31@users.noreply.github.com>
2023-01-18 08:45:34 +0000

    chore: add release banner for Jan 2023 CPU release (#1404)
    * Update banner for January CPU
    
    * Update src/components/Banner.tsx
    
    Co-authored-by: Wen Zhou <wenzhou@redhat.com>

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const Banner = () => {
-  return null;
+//  return null;
 
-  // return (
-  //   <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-  //    <strong className='p-1'>19th of October 2022:</strong>
-  //       We are creating the October 2022 CPU binaries for Eclipse Temurin 8u352, 11.0.17, 17.0.5 and 19.0.1
-  //       You can track progress
-  //       <a
-  //         href="https://github.com/adoptium/adoptium/issues/180"
-  //         target='_blank'
-  //         rel='noopener noreferrer'
-  //         className="alert-link p-1 text-white"
-  //       >by platforms</a>.
-  //    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  //   </div>
-  // );
+   return (
+     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+      <strong className='p-1'>18th of January 2023:</strong>
+         We are creating the January 2023 CPU binaries for Eclipse Temurin 8u362, 11.0.18, 17.0.6 and 19.0.2
+         You can track progress
+         <a
+           href="https://github.com/adoptium/adoptium/issues/202"
+           target='_blank'
+           rel='noopener noreferrer'
+           className="alert-link p-1 text-white"
+         >by platforms</a>.
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+     </div>
+   );
 };
 
 export default Banner;

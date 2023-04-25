a840a0a1ac6f6db4ff35105a54769373e7ffbf88
George Adams <georgeadams1995@gmail.com>
2023-04-25 12:26:21 +0100

    fix HSTS header does not contain includeSubDomains (#1718)
    * fix HSTS header does not contain includeSubDomains
    
    * fix test case

--- src/pages/temurin/__tests__/nightly.test.tsx
+++ src/pages/temurin/__tests__/nightly.test.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest'
+import { act, screen, fireEvent } from '@testing-library/react';
+import { describe, expect, it } from 'vitest'
 import { axe } from 'vitest-axe';
 import Nightly, { Head } from '../nightly';
 
 describe('Temurin Nightly page', () => {
-  it('renders correctly', () => {
+  it('renders correctly', async () => {
     const { container } = render(<Nightly />);
     // eslint-disable-next-line
     const pageContent = container.querySelector('main');
+
+    await act(async () => {
+      const datepicker = screen.getByLabelText('Build Date')
+      fireEvent.change(datepicker, { target: { value: '01/01/2022' } })
+      expect(datepicker.getAttribute('value')).toBe('01/01/2022');
+    });
 
     expect(pageContent).toMatchSnapshot();
   });

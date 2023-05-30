2ab9b134e3906a397276707d3c0e51ac3a1a7acd
Tayebwa Noah <58124613+Ndacyayisenga-droid@users.noreply.github.com>
2023-05-31 02:04:45 +0300

    Show Brand under documentation (#1821)
    * Show Brand under documentation
    
    * fix tests

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -102,6 +102,7 @@ const DocumentationPage = ({ data }) => (
                 { name: 'Frequently Asked Questions', link: '/docs/faq' },
                 { name: 'Adoptium API', link: 'https://api.adoptium.net' },
                 { name: 'Adoptium Blog', link: 'https://adoptium.net/blog' },
+                { name: 'Brand', link: '/docs/brand' },
                 { name: 'Logo Styleguide', link: '/docs/logo-styleguide' }
               ]}
             />

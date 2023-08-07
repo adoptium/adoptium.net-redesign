1f0d94d7f2003beda8f891b16a75fdaa68e9a43e
George Adams <georgeadams1995@gmail.com>
2023-08-07 08:42:13 +0100

    remove release banner (#2049)

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
 const Banner = () => {
-  // return null;
+  return null;
 
   // The following is an example that can be used for future code freezes
   // Comment Out The Above Line ( return null ; ) and uncomment the below
 
-  return (
-    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-    <strong className='p-1'>18th of July 2023:</strong>
-        We are creating the July 2023 PSU binaries for Eclipse Temurin 8u382, 11.0.20, 17.0.8 and 20.0.2 
-        You can track progress
-        <a
-          href="https://github.com/adoptium/adoptium/issues/239"
-          target='_blank'
-          rel='noopener noreferrer'
-          className="alert-link p-1 text-white"
-        >by platforms</a>.
-     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-  );
+  // return (
+  //   <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+  //   <strong className='p-1'>18th of July 2023:</strong>
+  //       We are creating the July 2023 PSU binaries for Eclipse Temurin 8u382, 11.0.20, 17.0.8 and 20.0.2 
+  //       You can track progress
+  //       <a
+  //         href="https://github.com/adoptium/adoptium/issues/239"
+  //         target='_blank'
+  //         rel='noopener noreferrer'
+  //         className="alert-link p-1 text-white"
+  //       >by platforms</a>.
+  //    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  //   </div>
+  // );
 };
 
 export default Banner;

cd9603e113eb4fc9ddd7bc1c752d6684825416b0
Xavier FACQ <xavierfacq@users.noreply.github.com>
2023-08-11 17:27:07 +0200

    Fix/adoptium 2086 uniformize extension of proposed download (#2088)
    * [adoptium-2086] Uniformize extension of proposed download
    
    * Fix tests to be all greeen
    
    * Keep same notation without ';' at end of line

--- src/util/__tests__/fetchExtension.test.tsx
+++ src/util/__tests__/fetchExtension.test.tsx
@@ -3,11 +3,11 @@ import { describe, expect, it } from 'vitest'
 
 describe("fetchExtension", () => {
     it("should convert file.tar.gz to tar.gz", () => {
-        expect(fetchExtension("file.tar.gz")).toBe("tar.gz");
+        expect(fetchExtension("file.tar.gz")).toBe(".tar.gz");
     });
 
     it("should convert file.tar.xz to tar.xz", () => {
-        expect(fetchExtension("file.tar.xz")).toBe("tar.xz");
+        expect(fetchExtension("file.tar.xz")).toBe(".tar.xz");
     });
 
     it("should convert file.zip to .zip", () => {
--- src/util/fetchExtension.tsx
+++ src/util/fetchExtension.tsx
@@ -2,9 +2,9 @@ export function fetchExtension (filename: string) {
   let extension = `.${filename.split('.').pop()}`
   // Workaround to prevent extension returning as .gz
   if (extension === '.gz') {
-    extension = 'tar.gz'
+    extension = '.tar.gz'
   } else if (extension === '.xz') {
-    extension = 'tar.xz'
+    extension = '.tar.xz'
   }
   return extension
 }

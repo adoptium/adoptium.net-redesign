2d2508632df8101e521e4e06a1d979be0c1d0fb5
Stewart X Addison <6487691+sxa@users.noreply.github.com>
2023-09-25 13:16:44 +0000

    Banner to mention JDK21 delay (#2253)
    * Banner to mention JDK21 delay
    
    Signed-off-by: Stewart X Addison <sxa@redhat.com>
    
    * Update src/components/Banner/index.tsx
    
    I added that in to try and make the formatting look better as it didn't look like there was a clear space before the hyperlink but sine it didn't seem to make a difference I'll take this change ;-)
    
    Co-authored-by: Shelley Lambert <slambert@gmail.com>
    
    ---------
    
    Signed-off-by: Stewart X Addison <sxa@redhat.com>
    Co-authored-by: Shelley Lambert <slambert@gmail.com>

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
 const Banner = () => {
-  return null;
+  // return null;
 
   // The following is an example that can be used for future code freezes
   // Comment Out The Above Line ( return null ; ) and uncomment the below
 
-  // return (
-  //   <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-  //   <strong className='p-1'>18th of July 2023:</strong>
-  //       We are creating the July 2023 PSU binaries for Eclipse Temurin 8u382, 11.0.20, 17.0.8 and 20.0.2 
-  //       You can track progress
-  //       <a
-  //         href="https://github.com/adoptium/adoptium/issues/239"
-  //         target='_blank'
-  //         rel='noopener noreferrer'
-  //         className="alert-link p-1 text-white"
-  //       >by platforms</a>.
-  //    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-  //   </div>
-  // );
+   return (
+     <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+     <strong className='p-1'>25th of September 2023:</strong>
+         We are awaiting access to the new Java 21's specification tests
+         before formally releasing Temurin 21. In the meantime
+         <a href="https://adoptium.net/blog/2023/08/early-access-builds/"> early
+         access builds</a> (untested and not for production use) can be downloaded
+         from <a href="https://adoptium.net/temurin/nightly/?version=21">here</a> or
+         from <a href="http://api.adoptium.net/">our API</a>.
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+     </div>
+   );
 };
 
 export default Banner;

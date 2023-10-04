bf93101254e79406e8b70cc689a95f14b6665956
George Adams <georgeadams1995@gmail.com>
2023-10-04 03:04:42 +0100

    fix SSRProvider warning on build (#2283)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,29 +1,26 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 import { Slice } from 'gatsby'
-import SSRProvider from 'react-bootstrap/SSRProvider'
 
 import './layout.scss'
 
 const Layout = ({ children }) => {
   return (
-    <SSRProvider>
-      <div
-        className='container-fluid p-0'
-        style={{
-          backgroundColor: 'var(--bg)',
-          color: 'var(--text-normal)',
-          transition: 'color 0.2s ease-out, background 0.2s ease-out'
-        }}
-      >
-        <Slice alias='navbar' />
-        <Slice alias='banner' />
-        <main>
-          {children}
-        </main>
-        <Slice alias='footer' />
-      </div>
-    </SSRProvider>
+    <div
+      className='container-fluid p-0'
+      style={{
+        backgroundColor: 'var(--bg)',
+        color: 'var(--text-normal)',
+        transition: 'color 0.2s ease-out, background 0.2s ease-out'
+      }}
+    >
+      <Slice alias='navbar' />
+      <Slice alias='banner' />
+      <main>
+        {children}
+      </main>
+      <Slice alias='footer' />
+    </div>
   )
 }
 

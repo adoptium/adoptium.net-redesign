c6eaa81d0abc33f2bf27eaa3074a9875ce0eee23
Tim Ellison <tellison@users.noreply.github.com>
2023-10-09 16:21:23 +0100

    Update Temurin delay blog (#2300)
    * Update Temurin delay blog
    * Update banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -8,11 +8,13 @@ const Banner = () => {
 
    return (
      <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-     <strong className='p-1'>28th of September 2023:</strong>
-         We are awaiting access to the new Java 21's specification tests
-         before formally releasing Eclipse Temurin 21.<br/>
-         For further information including how to use our early access builds, please
-         read <a href="/blog/2023/09/temurin21-delay">our blog</a>.
+     <strong className='p-1'>9th October 2023:</strong>
+         We have received the new Java 21 specification tests
+         and expect to
+         formally <a href="https://github.com/adoptium/temurin/issues/8">release Eclipse Temurin 21 shortly</a>.
+         <br/>
+         For further information, please read the update
+         in <a href="/blog/2023/09/temurin21-delay">our blog</a>.
       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
      </div>
    );

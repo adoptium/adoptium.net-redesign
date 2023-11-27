2d48652c5bea4685470363d32dad8b909c1411cc
George Adams <georgeadams1995@gmail.com>
2023-11-27 21:26:24 +0000

    latest temurin hook

--- src/components/LatestTemurin/index.tsx
+++ src/components/LatestTemurin/index.tsx
@@ -1,8 +1,8 @@
 import React, { MutableRefObject, useRef } from "react"
 import { Link, Trans } from "gatsby-plugin-react-i18next"
-import { FaArrowCircleRight, FaArchive, FaDownload } from "react-icons/fa"
 import { detectOS, UserOS } from "../../util/detectOS"
 import { fetchLatestForOS, useOnScreen } from "../../hooks"
+import { FaApple, FaWindows } from "react-icons/fa"
 
 let userOSName: string
 let userOSAPIName: string
@@ -61,79 +61,60 @@ const LatestTemurin = (props): JSX.Element => {
   }
 
   return (
-    <div
-      ref={ref}
-      className={
-        props.page === "home" ? "container hide-on-mobile" : "container"
-      }
-    >
-      {binary ? (
-        <>
-          <h2 className={`fw-light mt-3 ${textClass}`}>
-            <Trans
-              i18nKey="Download Temurin for"
-              userOSName={userOSName}
-              arch={arch}
-            >
-              Download Temurin&trade; for {{ userOSName }} {{ arch }}
-            </Trans>
-          </h2>
-          {isSafari && (
-            <span>
-              We have detected that you're using Safari. Because we are{" "}
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://stackoverflow.com/a/65412357"
-              >
-                unable to detect your architecture
-              </a>{" "}
-              we have suggested aarch64 (M1/M2). For x64 builds please see other{" "}
-              <Link to="/temurin/releases">downloads</Link>.
-            </span>
-          )}
-        </>
-      ) : (
-        <h2 className={`fw-light mt-3 ${textClass}`}>
-          Download Temurin&trade;
-        </h2>
-      )}
-      <div className={`btn-group-vertical mx-auto ${buttonClass}`}>
+    <div ref={ref} className="text-center w-full">
+      <h1 className="font-semibold leading-[72px] lg:leading-[120px] text-white-900 text-[64px] lg:text-[104px]">
+        The Power of Eclipse Temurin™
+      </h1>
+      <p className="lg:my-10 mt-6  mb-10 text-2xl leading-8 text-white-600  font-semibold">
+        <Trans
+          i18nKey="Download Temurin for"
+          userOSName={userOSName}
+          arch={arch}
+          version={defaultVersion}
+        >
+          Download Temurin {{ defaultVersion }} for {{ userOSName }} {{ arch }}
+        </Trans>
+      </p>
+      <div className="mt-10 flex items-center sm:flex-row flex-col-reverse justify-center gap-6">
+        <Link
+          to="/temurin/releases"
+          className="text-base underline transition duration-300 ease-in-out font-bold leading-6 text-white-900"
+        >
+          Other Downloads
+        </Link>
         {binary ? (
-          <>
-            <Link
-              to="/download"
-              state={{
-                link: binary.link,
-                checksum: binary.checksum,
-                os: userOSName,
-                arch: arch,
-                pkg_type: "JDK",
-                java_version: binary.release_name,
-              }}
-              className="btn btn-lg btn-primary mt-3 py-3 text-white"
-            >
-              <FaDownload /> <Trans>Latest LTS Release</Trans>
-              <br />
-              <span style={{ fontSize: ".6em" }} className="font-weight-light">
-                {binary.release_name}
-              </span>
-            </Link>
-          </>
-        ) : (
           <Link
-            to="/temurin/releases"
-            className="btn btn-lg btn-primary mt-3 py-3 text-white"
+            to="/download"
+            state={{
+              link: binary.link,
+              checksum: binary.checksum,
+              os: userOSName,
+              arch: arch,
+              pkg_type: "JDK",
+              java_version: binary.release_name,
+            }}
+            className="rounded-[80px] bg-[#FF1464] hover:bg-transparent border transition duration-300 ease-in-out hover:text-[#FF1464] border-[#FF1464] flex items-center justify-center gap-3 w-[244px] h-[56px] text-white font-bold leading-6 text-base"
           >
-            <FaDownload /> Latest LTS releases
+            {userOS === UserOS.MAC ? (
+              <FaApple size={25} />
+            ) : (
+              <FaWindows size={25} />
+            )}
+            Download Temurin™
           </Link>
+        ) : (
+          <a
+            href="#"
+            className="rounded-[80px] bg-[#FF1464] cursor-not-allowed hover:bg-transparent border transition duration-300 ease-in-out hover:text-[#FF1464] border-[#FF1464] flex items-center justify-center gap-3 w-[244px] h-[56px] text-white font-bold leading-6 text-base"
+          >
+            {userOS === UserOS.MAC ? (
+              <FaApple size={25} />
+            ) : (
+              <FaWindows size={25} />
+            )}
+            Download Temurin™
+          </a>
         )}
-        <Link to="/temurin/releases" className="btn btn-outline-dark mt-3">
-          <Trans>Other platforms and versions</Trans> <FaArrowCircleRight />
-        </Link>
-        <Link to="/temurin/archive" className="btn btn-outline-dark mt-3">
-          <Trans>Release Archive</Trans> <FaArchive />
-        </Link>
       </div>
     </div>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 
-import { AppleIcon, HeroBgImg } from "../components/Common/Icon"
+import { HeroBgImg } from "../components/Common/Icon"
 
+import LatestTemurin from "../components/LatestTemurin"
 import LogoCarousel from "../components/LogoCarousel"
 import PowerOfTemurin from "../components/Temurin/PowerOfTemurin"
 import DownloadCounter from "../components/Temurin/DownloadCounter"
@@ -16,7 +17,7 @@ import LatestNews from "../components/LatestNews"
 import FAQ from "../components/FAQ"
 import Contributors from "../components/Contributors/home"
 
-const Index = () => {
+const Index = ({ data }) => {
   return (
     <Layout>
       <div className="bg-purple sm:bg-contain bg-img-hero bg-center bg-no-repeat relative">
@@ -30,31 +31,7 @@ const Index = () => {
           ></div>
           <div className="max-w-[1264px] mx-auto flex justify-center items-center">
             <div className="mx-auto max-w-[832px] w-full h-[680px] sm:h-[720px] px-8 lg:px-0 flex items-center">
-              <div className="text-center w-full">
-                <h1 className="font-semibold leading-[72px] lg:leading-[120px] text-white-900 text-[64px] lg:text-[104px]">
-                  The Power of Eclipse Temurin™
-                </h1>
-                <p className="lg:my-10 mt-6  mb-10 text-2xl leading-8 text-white-600  font-semibold">
-                  Download Temurin™ for macOS aarch64
-                </p>
-                <div className="mt-10 flex items-center sm:flex-row flex-col-reverse justify-center gap-6">
-                  <a
-                    href="#"
-                    className="text-base underline transition duration-300 ease-in-out font-bold leading-6 text-white-900"
-                  >
-                    Learn More
-                  </a>
-                  <a
-                    href="#"
-                    className="rounded-[80px] bg-[#FF1464] hover:bg-transparent border transition duration-300 ease-in-out hover:text-[#FF1464] border-[#FF1464] flex items-center justify-center gap-3 w-[244px] h-[56px] text-white font-bold leading-6 text-base"
-                  >
-                    <span>
-                      <AppleIcon />
-                    </span>
-                    Download Temurin™
-                  </a>
-                </div>
-              </div>
+              <LatestTemurin latestLTS={data.mostRecentLts.version} />
             </div>
           </div>
         </div>

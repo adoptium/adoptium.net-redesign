61eb5e019dd113043c7311ff59068e3e473652a3
George Adams <georgeadams1995@gmail.com>
2023-12-08 19:39:04 +0000

    more tweaks

--- src/components/Logos/index.tsx
+++ src/components/Logos/index.tsx
@@ -44,7 +44,7 @@ const Logos = ({ members, title, description }: LogosGridProps) => {
   filteredMembers = shuffle(filteredMembers)
 
   return (
-    <section className="max-w-[1264px] md:px-0 px-6 w-full mx-auto flex flex-col items-center justify-center py-20">
+    <section className="max-w-[1264px] md:px-0 px-6 w-full mx-auto flex flex-col items-center justify-center py-10">
       <div className="max-w-[700px] mx-auto">
         <h3 className="text-[36px] sm:text-[42px] leading-[56px] text-center">
           {title}
@@ -54,51 +54,7 @@ const Logos = ({ members, title, description }: LogosGridProps) => {
         </p>
       </div>
       <div className="w-full flex flex-col items-center justify-center mt-3">
-        {members === LogoType.ALL && (
-          <div className="overflow-auto px-3 py-2 w-full md:w-auto">
-            <h3 className="flex space-x-10 whitespace-nowrap  justify-center py-2">
-              <a href="#strategic">
-                <span
-                  className="text-[16px] py-2 border-primary w-full text-base font-normal leading-6
-                outline-none hover:text-white hover:border-b
-                hover:border-[#ff1464] text-[#8a809e] border-b
-                border-transparent cursor-pointer transition-all duration-200 ease-in-out"
-                >
-                  Strategic Members
-                </span>
-              </a>
-              <a href="#enterprise">
-                <span
-                  className="text-[16px] py-3 border-primary w-full text-base font-normal leading-6
-                outline-none hover:text-white hover:border-b
-                hover:border-[#ff1464] text-[#8a809e] border-b
-                border-transparent cursor-pointer transition-all duration-200 ease-in-out"
-                >
-                  Enterprise Members
-                </span>
-              </a>
-              <a href="#participant">
-                <span
-                  className="text-[16px] py-2 border-primary w-full text-base font-normal leading-6
-                outline-none hover:text-white hover:border-b
-                hover:border-[#ff1464] text-[#8a809e] border-b
-                border-transparent cursor-pointer transition-all duration-200 ease-in-out"
-                >
-                  Participant Members
-                </span>
-              </a>
-            </h3>
-          </div>
-        )}
         <LogosGrid logos={filteredMembers} type={members} />
-      </div>
-      <div className="flex justify-center flex-wrap items-center gap-5 md:gap-14 mt-10">
-        <p className="text-[20px] leading-[28px] text-white my-0 text-center">
-          Are you interested in becoming a member?
-        </p>
-        <button className="rounded-2xl bg-transparent gradient-border border-2 border-pink-500/0 text-white text-base leading-6 font-normal w-[179px] h-[48px]  transition-all duration-500 ease-in-out ">
-          Become a Member
-        </button>
       </div>
     </section>
   )
--- src/components/LogosGrid/index.tsx
+++ src/components/LogosGrid/index.tsx
@@ -15,9 +15,7 @@ const LogosGrid = ({ logos, type }) => {
               <div className="grid grid-cols-4 gap-4 md:gap-6 min-w-[850px] md:min-w-[1220px]">
                 {logos.map(data => (
                   <a key={data.name} href={data.url}>
-                    <div
-                      className="relative w-[200px] md:w-[296px] h-[86px] md:h-[112px] flex justify-center items-center bg-white bg-opacity-50 rounded-3xl border border-white border-opacity-50 backdrop-blur-xl px-4 py-5"
-                    >
+                    <div className="relative w-[200px] md:w-[296px] h-[86px] md:h-[112px] flex justify-center items-center bg-white rounded-3xl border border-white border-opacity-50 backdrop-blur-xl px-4 py-5">
                       <img
                         src={`/images/${data.logo}`}
                         alt={data.name}

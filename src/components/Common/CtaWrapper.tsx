f37aeb36d77a19e371e799dca32a6e9d8389ddb9
Tanu Chaudhary <kaurtanu213@gmail.com>
2024-02-10 15:12:45 +0530

    support us

---
+++ src/components/Common/CtaWrapper.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const CtaWrapper = () => {
+  return (
+    <div className="max-w-[1264px] mx-auto bg-[#200E46] border-white p-8"></div>
+  )
+}
+
+export default CtaWrapper
---
+++ src/pages/latestReleases.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const LatestReleases = () => {
+  return <div className="text-white"> </div>
+}
+
+export default LatestReleases

f240c3b1b1f3e9d872aa78f98593f7bb4f1df6f2
Tanu Chaudhary <kaurtanu213@gmail.com>
2024-02-11 21:29:13 +0530

    latest page with reponsiveness

--- src/pages/latestReleases.tsx
+++ src/pages/latestReleases.tsx
@@ -1,47 +1,41 @@
 import React from "react"
+import CtaWrapper from "../components/Common/CtaWrapper"
+import Layout from "../components/Layout"
+import NavBar from "../components/NavBar"
+import PageHeader from "../components/PageHeader"
+import FAQ from "../components/FAQ"
+import { DownloadIcon } from "../components/Common/AppIcon"
 
-import { WindowIcon } from "../components/Common/AppIcon"
-import OperatingSystemSelector from "../components/Common/OperatingSystemSelector"
-import CommonDownloader from "../components/Common/CommonDownloader"
-
-const LatestReleases = () => {
-  const arr = [
-    {
-      label: "MSI, 185.41Mb",
-      sha: "SHA1",
-      file: "",
-    },
-
-    {
-      label: "ZIP, 188.84Mb",
-      sha: "SHA1",
-      file: "",
-    },
-
-    {
-      label: "ZIP, 188.84Mb",
-      sha: "SHA1",
-      file: "",
-    },
-  ]
-
+const latestReleases = () => {
   return (
-    <div className="">
-      <div className="max-w-[1264px] space-y-6  w-full  mt-5  mx-auto py-8 rounded-[24px] bg-[#200E46] border-white p-8">
-        <div className="flex justify-between items-center">
-          <OperatingSystemSelector
-            operatingSystem={"Window"}
-            svgComponent={<WindowIcon />}
-          />
-          <div className="flex flex-col">
-            {arr.map(obj => (
-              <CommonDownloader obj={obj} />
-            ))}
+    <div>
+      <Layout>
+        <NavBar />
+        <PageHeader
+          title={"Download Liberica JDK"}
+          subtitle={"Latest Relseases"}
+          description={
+            "Pick a version, package type, JDK/JRE, and download the binaries."
+          }
+        />
+        <section className="py-16 ">
+          <h2 className="text-center text-4xl mb-10 font-semibold md:text-5xl leading-[116%] ">
+            Latest Temurin
+          </h2>
+          <div className="flex justify-center items-center">
+            <button className="bg-transparent  flex items-center justify-center  border-2 border-pink-500/0 text-white text-base leading-6 font-bold w-[191px] h-[48px] rounded-2xl gradient-border">
+              Source Code{" "}
+              <span className="ml-2">
+                <DownloadIcon />
+              </span>
+            </button>
           </div>
-        </div>
-      </div>
+          <CtaWrapper />
+        </section>
+        <FAQ />
+      </Layout>
     </div>
   )
 }
 
-export default LatestReleases
+export default latestReleases

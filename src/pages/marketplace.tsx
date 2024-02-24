606931a598fbf9e009ff8d51c33687be465b8ebb
rohit <rohit780790809@gmail.com>
2024-02-24 14:39:08 +0530

    MarketPlace page added

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -4,61 +4,20 @@ import { Trans } from "gatsby-plugin-react-i18next"
 
 import Layout from "../components/Layout"
 import Seo from "../components/Seo"
-import DownloadDropdowns from "../components/DownloadDropdowns"
-import DownloadTable from "../components/MarketplaceDownloadTable"
-import { getAllPkgsForVersion } from "../hooks"
-import ChecksumModal from "../components/ChecksumModal"
-import LinkText from "../components/LinkText"
+import PageHeader from "../components/PageHeader"
+import MarketPlaceAll from "../components/MarketPlace/MarketPlaceAll"
+import AboutEclipseCard from "../components/MarketPlace/AboutEclipseCard"
 
 const DownloadPage = () => (
   <Layout>
-    <section className="py-5 text-center container">
-      <div className="row py-lg-5">
-        <div className="col-lg-8 col-md-8 mx-auto">
-          <h1 className="fw-light">Adoptium&reg; Marketplace</h1>
-          <p className="pt-3">
-            <Trans shouldUnescape i18nKey="marketplace.marketplaceDescription">
-              Java&trade; is the world's leading programming language and
-              platform. The Adoptium Marketplace promotes high-quality, TCK
-              certified and AQAvit verified runtimes for use across the Java
-              ecosystem.
-            </Trans>
-          </p>
-          <div className="row align-items-center pt-3">
-            <div className="col-6 col-md-4">
-              <img
-                src="/images/aqavit-light.png"
-                width={150}
-                alt="AQAvit logo"
-                className="img-fluid"
-              />
-            </div>
-            <div className="col-12 col-sm-6 col-md-8 ">
-              <p className="text-start">
-                <Trans
-                  i18nKey="marketplace.aqavitDescription"
-                  defaults="The AQAvit open source test suite (<strong>A</strong>doptium <strong>Q</strong>uality <strong>A</strong>ssurance) can be found <aqavitLink>here</aqavitLink>. There is also a <blogPostLink>blog post and brief presentation</blogPostLink> that explains what testing is run and how it fits into the overall delivery pipeline."
-                  components={{
-                    aqavitLink: (
-                      <LinkText href="https://github.com/Adoptium/aqa-tests" />
-                    ),
-                    blogPostLink: (
-                      <LinkText href="https://blog.adoptopenjdk.net/2017/12/testing-java-help-count-ways" />
-                    ),
-                  }}
-                />
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <DownloadDropdowns
-        updaterAction={getAllPkgsForVersion}
-        marketplace
-        Table={DownloadTable}
-      />
-      <ChecksumModal />
-    </section>
+    <PageHeader
+      subtitle="Marketplace"
+      title="Adoptium® Marketplace"
+      description="Java™ is the world's leading programming language and platform. The Adoptium Marketplace promotes high-quality, TCK certified and AQAvit verified runtimes for use across the Java ecosystem."
+      className={"mx-auto max-w-[1048px] px-2 w-full"}
+    />
+    <MarketPlaceAll />
+    <AboutEclipseCard />
   </Layout>
 )
 

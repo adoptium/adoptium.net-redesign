06485e71c1fe4e3238bf689c0adc7c5ef63a326d
Xavier FACQ <xavierfacq@users.noreply.github.com>
2024-03-14 15:23:10 +0100

    Add riscv64 entry in Architecture selector (#2715)

--- src/util/defaults.tsx
+++ src/util/defaults.tsx
@@ -1,7 +1,7 @@
 // This file is used to store defaults which can be accessed by other Javascript/Typescript functions
 
 export const oses = ['Linux', 'alpine-linux', 'Windows', 'mac', 'AIX', 'Solaris'];
-export const arches = ['x64', 'x86', 'aarch64', 's390x', 'ppc64le', 'ppc64', 'arm', 'sparcv9'];
+export const arches = ['x64', 'x86', 'aarch64', 's390x', 'ppc64le', 'ppc64', 'arm', 'sparcv9' ,'riscv64'];
 export const packageTypes = ['JDK', 'JRE'];
 
 let defaultPackageType = 'jdk';

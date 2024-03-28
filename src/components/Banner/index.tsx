d6be7340bd5c52ab4ed3dd2d3acad0462377bad7
Shelley Lambert <slambert@gmail.com>
2024-03-28 13:30:44 -0400

    Remove banner (#2750)
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const Banner = () => {
-  // return null;
+  return null;
 
   // The following is an example that can be used for future banner alert
   // Comment Out The Above Line ( return null ; ) and uncomment the below
 
-    return (
-      <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-      <strong className='p-1'>20th March 2024:</strong>
-          We are creating the March 2024 PSU binaries for Eclipse Temurin 22.0.0<br/>
-          You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/35">by platform</a>.
-       <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    );
+  //  return (
+  //    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+  //    <strong className='p-1'>20th March 2024:</strong>
+  //        We are creating the March 2024 PSU binaries for Eclipse Temurin 22.0.0<br/>
+  //        You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/35">by platform</a>.
+  //     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  //    </div>
+  //  );
 
 };
 

0bb3395c6896bdb254bc0e4d6eba8bb4f383a3c4
Xavier FACQ <xavierfacq@users.noreply.github.com>
2024-04-18 15:34:21 +0200

    Remove the return null at first line ;-) (#2792)

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 
 const Banner = () => {
-  return null;
-
   // The following is an example that can be used for future banner alert
   // Comment Out The Above Line ( return null ; ) and uncomment the below
 
-    return (
-      <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
-      <strong className='p-1'>16th April 2024:</strong>
-          We are creating the April 2024 PSU binaries for Eclipse Temurin 8u412, 11.0.23, 17.0.11, 21.0.3 and 22.0.1<br/>
-          You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/38">by platform</a>.
-        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
-    );
-
+  return (
+    <div className="alert text-white alert-dismissible fade show mb-0 text-center" style={{ backgroundColor: '#ff1464' }} role="alert">
+    <strong className='p-1'>16th April 2024:</strong>
+        We are creating the April 2024 PSU binaries for Eclipse Temurin 8u412, 11.0.23, 17.0.11, 21.0.3 and 22.0.1<br/>
+        You can track progress <a className='alert-link p-1 text-white' href="https://github.com/adoptium/temurin/issues/38">by platform</a>.
+      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  );
 };
 
 export default Banner;

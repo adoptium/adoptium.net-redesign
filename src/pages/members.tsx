0c1ae72ed2db6f97ac2693fc9c5300545fd8af65
Jessy Ssebuliba <jessiessebuliba@gmail.com>
2025-04-02 13:13:29 +0300

    add tests
    Signed-off-by: Jessy Ssebuliba <jessiessebuliba@gmail.com>

--- src/pages/members.tsx
+++ src/pages/members.tsx
@@ -11,21 +11,6 @@ import Logos, { LogoType } from "../components/Logos"
 import { scrollToSection } from "../util/scrollToView"
 
 const Members = () => {
-  const scrollToSection = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    sectionId: string,
-  ) => {
-    event.preventDefault()
-    const section = document.getElementById(sectionId)
-    if (section) {
-      const navbarHeight = 98
-      window.scrollTo({
-        top: section.offsetTop - navbarHeight,
-        behavior: "instant",
-      })
-    }
-  }
-
   return (
     <Layout>
       <PageHeader

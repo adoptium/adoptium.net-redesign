3cb6c2022eb4c4498c49e7ffd2ee3cf2038da51d
Xavier FACQ <xavier.facq@free.fr>
2025-05-13 20:53:30 +0200

    [issue-968] Add missing text on the download page

--- src/components/WorkingGroup/Support/index.tsx
+++ src/components/WorkingGroup/Support/index.tsx
@@ -1,46 +1,46 @@
 import React from "react"
 import "./support.scss"
 import CommonHeading from "../../Common/CommonHeading"
+import CommonCard from "../../Common/CommonCard"
+import { RedIcon } from "../../Common/Icon"
+import { Link } from "../../Link"
 
 const Support = () => {
+    const media = 
+        {
+          title: "Media & Promotion",
+          description:
+            "Follow and promote the latest updates from the Eclipse Adoptium Project",
+          button: "Learn More",
+          href: "/news/",
+        }
+    
+
     return (
         <section className="pt-0 md:pt-14 md:pb-28">
             <div className="pt-12 pb-8 md:pb-16 max-w-[1048px] flex-col lg:flex-row mx-auto w-full px-6 lg:px-2 xl:px-0 flex justify-center gap-16 items-center">
                 <div className=" w-full lg:w-[55%] text-center lg:text-left">
                     <CommonHeading
-                        title={"Support the Elcipse Adoptium Working Group "}
+                        title={"Support Temurin & Secure Open Source Java for Everyone!"}
                         description={
-                            "Lorem ipsum dolor sit amet consectetur. Ante cursus adipiscing feugiat aliquam. Adipiscing nulla arcu turpis tortor leo integer justo. Ut turpis adipiscing aliquam phasellus aliquam. Lectus etiam dictum nunc magna gravida bibendum nibh aliquam. Eget arcu neque cursus mattis sollicitudin malesuada urna ridiculus metus. Pharetra volutpat neque fames gravida tellus."
+                            "Eclipse Temurin provides high-quality, open-source Java runtimes trusted by millions. By becoming a Temurin Sustainer, you are ensuring continued innovation, security, and accessibility for Java developers worldwide."
                         }
                         className={undefined}
                     />
-                    <CommonHeading
-                        description={
-                            "Nibh arcu consequat aliquet in eget non. Turpis id posuere quam sit magnis. Aliquam imperdiet odio odio vulputate. Vel molestie porttitor tellus accumsan. Dui ipsum fringilla sit sit elementum."
-                        }
-                        title={undefined}
-                        className={undefined}
-                    />
-                    <button className="text-white text-base leading-6 font-normal bg-primary px-6 py-3 mt-8 rounded-[80px]">
-                        Donate Now
-                    </button>
+                    <h2 className="text-2xl leading-[122%] md:text-2xl md:[116%] text-white font-semibold pt-8">Why Fund Temurin?</h2>
+                    <div>
+                        <div className="text-grey justify-start inline-flex pt-2"><div className="pt-1"><RedIcon /></div><div className="pl-2">Help keep Eclipse Temurin free and accessible to all.</div></div>
+                        <div className="text-grey justify-start inline-flex pt-2"><div className="pt-1"><RedIcon /></div><div className="pl-2">Maintain and grow infrastructure, security updates, and testing for the Java community.</div></div>
+                        <div className="text-grey justify-start inline-flex pt-2"><div className="pt-1"><RedIcon /></div><div className="pl-2">Support open source contributors maintaining Temurin.</div></div>
+                    </div>          
+                    <h2 className="text-2xl leading-[122%] md:text-2xl md:[116%] text-white font-semibold pt-8">How You Can Help</h2>
+                    <div>
+                        <div className="text-grey justify-start inline-flex pt-2"><div className="pt-1"><RedIcon /></div><div className="pl-2">Monthly Sustainer: Contribute as an individual sustainer to ensure via <a href="https://github.com/sponsors/adoptium" className="text-pink" rel="noopener noreferrer" target="_blank">**GitHub**</a> Sponsorship Temurin’s long-term viability, enhance security, and accelerate faster releases for the Java community</div></div>
+                        <div className="text-grey justify-start inline-flex pt-2"><div className="pt-1"><RedIcon /></div><div className="pl-2">One-Time Contributions – Help us scale our testing and infrastructure.</div></div>
+                        <div className="text-grey justify-start inline-flex pt-2"><div className="pt-1"><RedIcon /></div><div className="pl-2">Corporate Sustainer – Looking to make a bigger impact? Join as a <Link to="/become-a-sustainer/" className="text-pink">Corporate Sustainer</Link>.</div></div>
+                    </div>          
                 </div>
-                <div>
-                    <div className="p-10 flex flex-col max-w-[400px] newscard-2 h-[440px] md:h-[480px] bg-[#200E46]">
-                        <div className="grow">
-                            <h3 className="text-white text-[30px] font-semibold  whitespace-nowrap  md:whitespace-normal leading-[120%] md:text-[40px]">
-                                Media & Promotion
-                            </h3>
-                            <p className="mt-6 text-xl text-lightgrey font-normal">
-                                Eclipse Temurin offers high-performance, cross-platform,
-                                open-source Java runtime
-                            </p>
-                        </div>
-                        <button className="bg-transparent border-2 border-pink-500/0 text-white text-base leading-6 font-normal w-[180px] h-[48px] rounded-[80px] gradient-supportus">
-                            Learn More
-                        </button>
-                    </div>
-                </div>
+                <CommonCard data={media} />
             </div>
         </section>
     )

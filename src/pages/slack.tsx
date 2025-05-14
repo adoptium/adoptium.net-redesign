21822d2870e4c732d832db2c2a533f5dc84ef998
Xavier FACQ <xavier.facq@free.fr>
2025-05-14 13:57:56 +0200

    [issue-971] Update Slack form and Join form

--- src/pages/slack.tsx
+++ src/pages/slack.tsx
@@ -9,7 +9,7 @@ const JoinPage = () => (
     <section className="py-5 text-center container">
       <div className="row py-lg-5">
         <div className="col-lg-8 col-md-8 mx-auto">
-          <h1 className="fw-light">Join our Slack channel!</h1>
+          <h1 className="fw-light text-4xl pb-3">Join our Slack channel!</h1>
         </div>
         <form
           action="https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxrFvVu49Nx0zQKZAfHe_43H3H6BOhRiRXayQjjAmlbnSJfg/formResponse"
@@ -18,18 +18,18 @@ const JoinPage = () => (
         >
           <div className="form-group">
             <label>
-              Email address
+              Email address<br />
               <input
                 autoComplete="email"
                 name="emailAddress"
                 placeholder="Your email"
                 type="email"
-                className="form-control"
+                className="formInput"
                 required
               />
             </label>
             <br />
-            <button className="btn btn-primary mt-3" type="submit">
+            <button className="bg-transparent   border-2 border-pink-500/0 text-white text-base leading-6 font-normal w-[180px] h-[48px] rounded-[80px] gradient-border" type="submit">
               Invite Me!
             </button>
           </div>
